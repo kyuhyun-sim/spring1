eb368172eaf11ff07f96191c20231cb1596e241e
kyuhyun-sim <zaq7974@naver.com>
2022-07-25 15:19:03 +0900

    컨트롤러 변수안 서비스 변수 추가

--- spring1/src/main/java/com/sparta/spring1/controller/FreeBoardController.java
+++ spring1/src/main/java/com/sparta/spring1/controller/FreeBoardController.java
@@ -1,6 +1,7 @@
 package com.sparta.spring1.controller;
 
 import com.sparta.spring1.domain.FreeBoardRepository;
+import com.sparta.spring1.service.FreeBoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class FreeBoardController {
 
     private final FreeBoardRepository FreeBoardRepository;
+    private final FreeBoardService freeBoardService;
 
 }

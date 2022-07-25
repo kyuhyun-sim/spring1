2da5965dbfed23926fceb9fd8fbd474f65e9758b
kyuhyun-sim <zaq7974@naver.com>
2022-07-25 14:41:06 +0900

    FreeBoard 자바형식 엔터티 테이블 생성

---
+++ spring1/src/main/java/com/sparta/spring1/domain/FreeBoard.java
@@ -0,0 +1,12 @@
+package com.sparta.spring1.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class FreeBoard {
+}

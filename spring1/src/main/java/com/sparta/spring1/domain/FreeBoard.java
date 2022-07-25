5a70879b026ad60971de4929cf90b2fceddf000b
kyuhyun-sim <zaq7974@naver.com>
2022-07-25 14:48:03 +0900

    FreeBoard 테이블 변수및 기본생정자 생성.

--- spring1/src/main/java/com/sparta/spring1/domain/FreeBoard.java
+++ spring1/src/main/java/com/sparta/spring1/domain/FreeBoard.java
@@ -3,10 +3,30 @@ package com.sparta.spring1.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @NoArgsConstructor
 @Getter
 @Entity
-public class FreeBoard {
+public class FreeBoard extends Timestamped{
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @Column(nullable = false)
+    private String author;
+
+    public FreeBoard(String title, String contents, String author){
+        this.title = title;
+        this.contents = contents;
+        this.author = author;
+    }
+
+
 }

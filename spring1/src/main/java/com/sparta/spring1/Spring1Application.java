7377f8e6029731869b7ede639dc3940bf5bc6939
kyuhyun-sim <zaq7974@naver.com>
2022-07-25 14:55:32 +0900

    JPA 값 변경시 즉시 반영 어노테이션 생성

--- spring1/src/main/java/com/sparta/spring1/Spring1Application.java
+++ spring1/src/main/java/com/sparta/spring1/Spring1Application.java
@@ -2,7 +2,9 @@ package com.sparta.spring1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Spring1Application {
 

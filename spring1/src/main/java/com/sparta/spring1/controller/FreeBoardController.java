1f2106600658a8e5bca514f526c7b0ea498ebc09
kyuhyun-sim <zaq7974@naver.com>
2022-07-25 15:24:29 +0900

    freeBoardRepository -> (F)reeBoardRepository로 값 변경 + Post 추가

--- spring1/src/main/java/com/sparta/spring1/controller/FreeBoardController.java
+++ spring1/src/main/java/com/sparta/spring1/controller/FreeBoardController.java
@@ -1,15 +1,25 @@
 package com.sparta.spring1.controller;
 
+import com.sparta.spring1.domain.FreeBoard;
 import com.sparta.spring1.domain.FreeBoardRepository;
+import com.sparta.spring1.domain.FreeBoardRequestDto;
 import com.sparta.spring1.service.FreeBoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class FreeBoardController {
 
-    private final FreeBoardRepository FreeBoardRepository;
+    private final FreeBoardRepository freeBoardRepository;
     private final FreeBoardService freeBoardService;
+
+    @PostMapping("/api/post")
+    public FreeBoard createFreeBoard(@RequestBody FreeBoardRequestDto requestDto){
+        FreeBoard freeBoard = new FreeBoard(requestDto);
+        return freeBoardRepository.save(freeBoard);
+    }
 
 }
